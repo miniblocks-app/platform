23f2f0ccb55c22240fcde39aefbf0ecbda190abd
mrmihi <your-repo-specific-mrmihiraj@gmail.com>
2025-03-13 21:14:08 +0530

    feat: blockly, flutter blocks & sample flutter proj added

--- src/components/blocks/BlocksWindow.tsx
+++ src/components/blocks/BlocksWindow.tsx
@@ -1,31 +1,99 @@
-import { DndContext, DragEndEvent } from '@dnd-kit/core';
-import { LogicBlocksPalette } from './LogicBlocksPalette';
-import { BlocksCanvas } from './BlocksCanvas';
+import {BlocklyWorkspace} from "react-blockly";
 import { useAppStore } from '../../store';
+import { javascriptGenerator } from 'blockly/javascript';
+import { useState } from "react";
+import "./customBlocks/custom_Blocks";
+import {flutterCategory} from "../../categories/flutter.ts";
 
 export const BlocksWindow = () => {
   useAppStore();
+  const [xml, ] = useState("");
+  const [javascriptCode, setJavascriptCode] = useState("");
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { over, active } = event;
-    
-    if (over && over.id === 'blocks-canvas') {
-      const blockType = (active.data?.current as any)?.type;
-      if (blockType) {
-        // Add block to canvas
-        console.log('Add block:', blockType);
-      }
-    }
+  const initialXml =
+      '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
+  const toolboxCategories = {
+    kind: "categoryToolbox",
+    contents: [
+      // {
+      //   kind: "category",
+      //   name: "Logic",
+      //   colour: "#5C81A6",
+      //   contents: [
+      //     {
+      //       kind: "block",
+      //       type: "controls_if",
+      //     },
+      //     {
+      //       kind: "block",
+      //       type: "logic_compare",
+      //     },
+      //   ],
+      // },
+      // {
+      //   kind: "category",
+      //   name: "Math",
+      //   colour: "#5CA65C",
+      //   contents: [
+      //     {
+      //       kind: "block",
+      //       type: "math_round",
+      //     },
+      //     {
+      //       kind: "block",
+      //       type: "math_number",
+      //     },
+      //   ],
+      // },
+      // {
+      //   kind: "category",
+      //   name: "Custom",
+      //   colour: "#5CA699",
+      //   contents: [
+      //     {
+      //       kind: "block",
+      //       type: "new_boundary_function",
+      //     },
+      //     {
+      //       kind: "block",
+      //       type: "return",
+      //     },
+      //   ],
+      // },
+      flutterCategory,
+    ],
   };
 
+  function workspaceDidChange(workspace) {
+    const code = javascriptGenerator.workspaceToCode(workspace);
+    setJavascriptCode(code);
+  }
+
+
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      <div className="flex-1 flex">
-        <div className="w-64 bg-white border-r flex flex-col">
-          <LogicBlocksPalette />
-        </div>
-        <BlocksCanvas />
-      </div>
-    </DndContext>
+      <>
+        <BlocklyWorkspace
+            toolboxConfiguration={toolboxCategories}
+            initialXml={initialXml}
+            className="[h-screen-100px] w-screen"
+            workspaceConfiguration={{
+              grid: {
+                spacing: 20,
+                length: 3,
+                colour: "#ccc",
+                snap: true,
+              },
+            }}
+            onWorkspaceChange={workspaceDidChange}
+            // onXmlChange={setXml}
+        />
+        <pre id="generated-xml">{xml}</pre>
+        <textarea
+            id="code"
+            style={{ height: "200px", width: "400px" }}
+            value={javascriptCode}
+            readOnly
+        ></textarea>
+      </>
   );
 };
